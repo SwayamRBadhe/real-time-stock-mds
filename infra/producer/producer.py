383a19eeb1b59d62f7528bcc9c268ccea519836e
SwayamRBadhe <badheswayam@gmail.com>
2026-01-31 18:30:01 -0500

    consumer file added. data sent from kadka to minio bucket

--- infra/producer/producer.py
+++ infra/producer/producer.py
@@ -1,17 +1,21 @@
+#import files required
 import time
 import json
 import requests
 from kafka import KafkaProducer
 
+#setup the variables for api
 API_KEY = "d5u9qlhr01qtjet2h3igd5u9qlhr01qtjet2h3j0"  #api key
 BASE_URL = "https://finnhub.io/api/v1/quote"            #url fomr which api recieved
 SYMBOLS = ["AAPL", "MSFT", "TSLA", "GOOGL", "AMZN"]     #5 companies data taken
 
+#initial kafkaproducer
 producer = KafkaProducer (
     bootstrap_servers = ["host.docker.internal:29092"],       #connecting to kafka. we can use normal localhost but we are running docker outside so taken entire link
     value_serializer = lambda v: json.dumps(v).encode("utf-8") #kafka streams the data in bytes. so we are converting the dictionary to json format then via utf-8 we are converting to bytes. 
 )
 
+#retreive data
 def fetch_quote(symbol):
     url = f"{BASE_URL}?symbol={symbol}&token={API_KEY}"     #this is the whole url symbol, token api key coz we are gonna fetch it
     try:                                                    #doing exception handling inorder to fetch the api
@@ -25,6 +29,7 @@ def fetch_quote(symbol):
         print(f"Error fetching {symbol}: {e}") #throw an error if not found
         return None
 
+#looping and pushing to stream of the data
 while True:
     for symbol in SYMBOLS:      #go through each company in SYMBOL array
         quote = fetch_quote(symbol)     #call the function for each symbol and return the symbol->(metadata)symbol and time
